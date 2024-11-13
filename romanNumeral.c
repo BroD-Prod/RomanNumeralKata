f0d849a025dc21aebd582d667d48618e89bc6489
BroD-Prod <brodyroth23@gmail.com>
2024-11-12 20:00:01 -0500

    Differ from conversion and exact value of number in roman numerals
    Change to return I for for number input and moved others a new function

--- romanNumeral.c
+++ romanNumeral.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-void romanNumeral(int k){
-    if (k % 1){
+#include <math.h>
+
+void romanNumeralConversion(int k){
+    while (k > 0) {
         printf("I");
-    }
-    if (k == 5){
-        printf("V");
-    }
-    if (k == 10){
-        printf("X");
-    }
-    if (k == 50){
-        printf("L");
-    }
-    if (k == 100){
-        printf("C");
-    }
-    if (k == 500){
-        printf("D");
-    }
-    if (k == 1000){
-        printf("M");
+        k -= 1;
     }
 }
 
@@ -27,6 +12,27 @@ int main(){
     int k = 0;
     printf("Please enter a number from 1 - 1300: ");
     scanf("%d", &k);
-    romanNumeral(k);
+    romanNumeralConversion(k);
     return 0;
+}
+
+void romanNumeralExact() {
+    if (k == 5) {
+        printf("V");
+    }
+    if (k == 10) {
+        printf("X");
+    }
+    if (k == 50) {
+        printf("L");
+    }
+    if (k == 100) {
+        printf("C");
+    }
+    if (k == 500) {
+        printf("D");
+    }
+    if (k == 1000) {
+        printf("M");
+    }
 }
