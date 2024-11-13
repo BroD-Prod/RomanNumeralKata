89b17e446ed8773117bf2c41789f9f2f3b1ec402
BroD-Prod <brodyroth23@gmail.com>
2024-11-12 20:34:54 -0500

    Return a roman numeral for any number between 1 and 1300
    Change to the function to return a roman numeral based on the number input

--- romanNumeral.c
+++ romanNumeral.c
@@ -3,12 +3,34 @@
 
 void romanNumeralConversion(int k){
     while (k > 0) {
-        if(k / 1000 == 1){
+        if(k >= 1000){
             printf("M");
             k -= 1000;
         }
-        printf("I");
-        k -= 1;
+        else if(k >= 500){
+            printf("D");
+            k -= 500;
+        }
+        else if(k >= 100){
+            printf("C");
+            k -= 100;
+        }
+        else if(k >= 50){
+            printf("L");
+            k -= 50;
+        }
+        else if(k >= 10){
+            printf("X");
+            k -= 10;
+        }
+        else if(k >= 5){
+            printf("V");
+            k -= 5;
+        }
+        else {
+            printf("I");
+            k -= 1;
+        }
     }
 }
 
@@ -16,7 +38,13 @@ int main(){
     int k = 0;
     printf("Please enter a number from 1 - 1300: ");
     scanf("%d", &k);
-    romanNumeralConversion(k);
+    if (k < 1300){
+        printf("That was not less than 1300, please enter a number from 1 - 1300: ");
+        scanf("%d", &k);
+    }
+    else {
+        romanNumeralConversion(k);
+    }
     return 0;
 }
 
